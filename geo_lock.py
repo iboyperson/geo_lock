c06fc2d1bac725c7464f3d466bf04a3ad7865715
Zrohn <zrohn@localhost.localdomain>
2017-07-21 19:46:44 -0400

    Added error checking

--- geo_lock.py
+++ geo_lock.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 import sys
 import pygeoip
+acceptables=[1,2,3,4,5,6,7,8,9,0,"."]	
 try:
-        ascii = """\
+	try:
+	        ascii = """\
   ______   ________   ______         __         ______    ______   __    __ 
  /      \ /        | /      \       /  |       /      \  /      \ /  |  /  |
 /$$$$$$  |$$$$$$$$/ /$$$$$$  |      $$ |      /$$$$$$  |/$$$$$$  |$$ | /$$/ 
@@ -14,17 +16,22 @@ $$    $$/ $$       |$$    $$/______ $$       |$$    $$/ $$    $$/ $$ | $$  |
  $$$$$$/  $$$$$$$$/  $$$$$$//      |$$$$$$$$/  $$$$$$/   $$$$$$/  $$/   $$/ 
                             $$$$$$/                                         
 """
-        print (' ')
-        print ('\x1b[1;32;40m'+ascii+'\x1b[0m')
-        try:
-                ip = sys.argv[1]
-        except:
-                pass
-                ip = raw_input('\x1b[1;33;40m'+'Input IP: '+'\x1b[0m')
-        gip = pygeoip.GeoIP('GeoLiteCity.dat')
-        rec = gip.record_by_addr(ip)
-        for val in rec.items():
-                print '==========================='
-                print '\x1b[1;33;40m'+'%s:''\x1b[0m''%s'%(val)
-except KeyboardInterrupt:
-    print(' ')
+	        print (' ')
+	        print ('\x1b[1;32;40m'+ascii+'\x1b[0m')
+	        try:
+	                ip = sys.argv[1]
+	        except:
+	                pass
+			try:
+		                ip = eval(int(raw_input('\x1b[1;33;40m'+'Input IP: '+'\x1b[0m')))
+			except ValueError:
+			    print ("Invalid Input")
+	        gip = pygeoip.GeoIP('GeoLiteCity.dat')
+	        rec = gip.record_by_addr(ip)
+	        for val in rec.items():
+	                print '==========================='
+	                print '\x1b[1;33;40m'+'%s:''\x1b[0m''%s'%(val)
+	except KeyboardInterrupt:
+		print(' ')
+except NameError:
+	print('Exiting...')
